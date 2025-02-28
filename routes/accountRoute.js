b060272f93156a95e6c177f430149c27b2a87c98
TJ Strickland <thomaskeith17@gmail.com>
2025-02-28 00:07:42 -0700

    week 07 assignments

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require("../utilities/account-validation")
 
 
 //Get route for path sent from the "My Account" link when clicked
@@ -11,7 +12,13 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Registration route
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 
 module.exports = router
